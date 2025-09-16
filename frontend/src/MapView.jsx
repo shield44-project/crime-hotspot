6da8a48306d490877e1aa01cf5e6621f3ead7836
shield44 <ksthejas060@gmail.com>
2025-09-16 22:20:07 +0530

    MapView: drop heatmap

--- frontend/src/MapView.jsx
+++ frontend/src/MapView.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup, useMap, Rectangle } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
-import "leaflet.heat";
 import "./styles/MarkerCluster.css";
 import "./styles/MarkerCluster.Default.css";
 import "./MapView.css";
@@ -30,18 +29,6 @@ const ResetViewButton = ({ bounds }) => {
   return <button className="reset-button" onClick={handleReset}>Reset View</button>;
 };
 
-const HeatmapLayer = ({ points }) => {
-  const map = useMap();
-  useEffect(() => {
-    if (!map) return;
-    const heat = L.heatLayer(points, { radius: 25, blur: 15, maxZoom: 12 });
-    heat.addTo(map);
-    return () => {
-      map.removeLayer(heat);
-    };
-  }, [map, points]);
-  return null;
-};
 
 const MapView = ({ crimes }) => {
   const [crimeBounds, setCrimeBounds] = useState(null);
@@ -166,9 +153,7 @@ const MapView = ({ crimes }) => {
       >
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
-        {/* Heatmap overlay using lat,lon,[intensity=1] */}
-        <HeatmapLayer points={validCrimes.map((c) => [c.Latitude, c.Longitude, 1])} />
-
+        
         {/* Region density overlay (dominant type color, opacity by density) */}
         {gridRects.map((g) => (
           <Rectangle
